dccfa6f9c86fee1752e45a1251980e2b3e8316bf
bendaleprashant <bendaleprashant@gmail.com>
2024-03-01 09:17:03 +0530

    Initial commit

---
+++ src/components/Records.js
@@ -0,0 +1,26 @@
+const Records = ({data}) => {
+    return(
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope='col'>UserID</th>
+                    <th scope='col'>Id</th>
+                    <th scope='col'>Title</th>
+                    <th scope='col'>Body</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(item => {
+                    <tr>
+                        <td>{item.userId}</td>
+                        <td>{item.id}</td>
+                        <td>{item.title}</td>
+                        <td>{item.body}</td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
+    )
+}
+
+export default Records
